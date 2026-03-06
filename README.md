5c8939fcf98763de25d3b108520e4834f3567f2e
Luca Iglesias <lucaiglesias@gmail.com>
2026-03-05 21:19:34 -0500

    chore: Created a test to open the browser on a specific page.

--- README.md
+++ README.md
@@ -3,5 +3,5 @@ Practice of automation testing using Java and Selenium to login in a web page.
 
 
 # 04-March-2026 -- Status: Project created!
-# 05-March-2026 -- Status: Added Selenium and Junit.
+# 05-March-2026 -- Status: Added Selenium and Junit. Tested to open the browser and open the page.
 
--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,5 +1,25 @@
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FirstTest {
-    WebDriver driver;
+
+    @Test
+    public void openBrowser() {
+        // 1. Instancia o driver do Chrome (O Selenium 4 baixa o driver sozinho!)
+        // 1. Instaciate Chrome's driver (Selenium download it by it self!)
+        WebDriver driver = new ChromeDriver();
+
+        // 2. Manda o navegador ir para o site
+        // 2. Tell the browser to open the webpage
+        driver.get("https://the-internet.herokuapp.com/login");
+
+        // 3. Maximiza a janela para a gente enxergar melhor
+        // 3. Make it full screen
+        driver.manage().window().maximize();
+
+        // 4. (Opcional) Fecha o navegador depois de 3 segundos para teste
+        // 4. (Optional) Close it after 3 seconds
+        driver.quit();
+    }
 }
